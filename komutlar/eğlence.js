04991546de03bcf07483a65d27f9cdaf2df61d08
Glitch (discordjsv12basitaltyapi) <none>
2020-11-03 12:32:10 +0000

    🕴👲 Checkpoint ./komutlar/yardım.js:24240389/252 ./komutlar/eğlence.js:24240389/252 ./komutlar/logo.js:24240389/328

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -7,10 +7,10 @@ exports.run = async (client, message, args) => {
 const eğlence = new Discord.MessageEmbed()
 .setColor("RANDOM")
 .setAuthor("Aleria Fun yeniden sizlerle.")
-.setTitle("<a:745864654450065479:757640512055541912> Aleria Eğlence Komutları <a:745864654450065479:757640512055541912>")
+.setTitle("<a:750341192008597684:772455369221734420> Aleria Eğlence Komutları <a:750341192008597684:772455369221734420>")
  .setTimestamp()
 .setDescription("<a:B_Kristal:749194671527428126> **-tersyazı** = Bir Yazıyı Bot Ters Yazar. <a:B_Kristal:749194671527428126> \n <a:B_Kristal:749194671527428126> **-mcskin** = Yazdığınız ismin minecraft görünüşünü atar. <a:B_Kristal:749194671527428126> \n <a:B_Kristal:749194671527428126> **-fbi** = Bot FBi Gif Atar. <a:B_Kristal:749194671527428126> \n <a:B_Kristal:749194671527428126> **-token** = Tokenimi Öğrenmek İstemezmisin? <a:developeremoji:749525084586115153> \n <a:B_Kristal:749194671527428126> **-düello** = Düello Atarsın. <a:B_Kristal:749194671527428126> \n <a:B_Kristal:749194671527428126>**-wasted** = Polis tarafından yakalanırsın. <a:B_Kristal:749194671527428126> \n <a:B_Kristal:749194671527428126> **-atatürk** = Dene ve gör... (1881-1938) <a:B_Kristal:749194671527428126>")
-.setImage("")
+.setImage("https://cdn.discordapp.com/attachments/769159402137059383/772838745467518997/unknown.png")
 message.channel.send(eğlence)
 }
 
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,10 +7,10 @@ exports.run = async (client, message, args) => {
 const yardım = new Discord.MessageEmbed()
 .setColor("BLUE")
 .setAuthor("Aleria Fun yeniden sizlerle.")
-.setTitle("<a:765109811611959316:767522721722269736> Aleria yardım menüsüne hoşgeldin! <a:765109811611959316:767522721722269736>")
+.setTitle("<a:750341192008597684:772455369221734420> Aleria yardım menüsüne hoşgeldin! <a:750341192008597684:772455369221734420>")
  .setTimestamp()
 .setDescription("<a:B_Alarm:749194316634652712> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:B_Alarm:749194316634652712> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:B_Alarm:749194316634652712> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:B_Alarm:749194316634652712> **-logo** = __Logo komutlarını görüntülersiniz.__")
-.setImage("")
+.setImage("https://cdn.discordapp.com/attachments/769159402137059383/772838745467518997/unknown.png")
 message.channel.send(yardım)
 }
 
